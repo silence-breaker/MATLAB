41861043cc74755936786383b69b97aa1dc8b109
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-16 11:52:26 +0800

    Auto update: 2025-09-16 11:52:26

---
+++ P1.m
@@ -0,0 +1,9 @@
+var=1+1i;
+arr=[1,2,3;4,5,6];
+arr1=[arr,arr];
+number = 1;
+my_val = input('Enter a number: ');
+disp(['You entered: ', num2str(my_val)]);
+arr2 = arr(2:end,2:end);
+arr4 = [1 2 3 4;5 6 7 8;9 10 11 12];
+arr3 = arr4(1:2,[1 4]); % 提取第一、二行，第一、四列
---
+++ init.m
@@ -0,0 +1,3 @@
+cd('d:/SYSU/课务/MATLAB计算与仿真/代码');
+disp("当前工作目录为：");
+disp(pwd);
